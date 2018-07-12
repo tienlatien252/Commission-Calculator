f8e8deadd58ff433b1e21289e28ca16da448755a
An Vo <thaiankh@gmail.com>
2018-07-12 00:32:05 -0500

    create dummy of employers using listViw

--- lib/employerSetup.dart
+++ lib/employerSetup.dart
@@ -7,13 +7,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:redux/redux.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
-
-class EmployersView{
+class EmployersView {
   EmployersView({this.employers});
   final List<Employer> employers;
 }
 
-class Employer{
+class Employer {
   const Employer({this.name, this.commissionRate});
   final String name;
   final double commissionRate;
@@ -28,13 +27,12 @@ class EmployerSetup extends StatefulWidget {
 }
 
 class _EmployerSetupState extends State<EmployerSetup> {
-
-  Future <WelcomePage> _saveEmployersAndGoNext()  async{
+  Future<WelcomePage> _saveEmployersAndGoNext() async {
     await FirebaseAuth.instance.signOut();
     return new WelcomePage();
   }
 
-  Future <WelcomePage> _openAddEmployerDialog()  async{
+  Future<WelcomePage> _openAddEmployerDialog() async {
     // TODO implement the dialog
   }
 
@@ -42,28 +40,28 @@ class _EmployerSetupState extends State<EmployerSetup> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text(widget.title),
+        title: new Text("Employer\'s setup"),
       ),
       body: Center(
         child: new Column(
-            //mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              // new Container(
-              //   height: 100.0,
-              //   child: new Column(
-              //     mainAxisAlignment: MainAxisAlignment.spaceAround,
-              //     children: <Widget>[
-              //       new Text('Employer\'s setup'),
-              //       new RaisedButton(
-              //         child: new Text('Add A New Employer'),
-              //         onPressed: _openAddEmployerDialog,
-              //       ),
-              //     ],
-              //   ),
-              // ),
-              new EmployersListView(),
-            ],
-          ),
+          //mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            new Container(
+              height: 100.0,
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  //new Text('Employer\'s setup'),
+                  new RaisedButton(
+                    child: new Text('Add A New Employer'),
+                    onPressed: _openAddEmployerDialog,
+                  ),
+                ],
+              ),
+            ),
+            new EmployersListView(),
+          ],
+        ),
       ),
       floatingActionButton: new FloatingActionButton(
         onPressed: _saveEmployersAndGoNext,
@@ -74,8 +72,8 @@ class _EmployerSetupState extends State<EmployerSetup> {
   }
 }
 
-class EmployersListView extends StatefulWidget{
-  EmployersListView ({Key key, this.user}) : super(key: key);
+class EmployersListView extends StatefulWidget {
+  EmployersListView({Key key, this.user}) : super(key: key);
   final FirebaseUser user;
 
   @override
@@ -83,23 +81,24 @@ class EmployersListView extends StatefulWidget{
 }
 
 class _EmployersListViewState extends State<EmployersListView> {
-
-    @override
+  @override
   Widget build(BuildContext context) {
     return new StoreConnector<AppState, List<Employer>>(
-      converter: (store)  {
-        Employer employer = new Employer(name: "an", commissionRate: 1.2);
+      converter: (store) {
+        Employer employer = new Employer(name: "Star Nails", commissionRate: 1.2);
         return [employer];
       }, 
       builder: (context, employers) {
-        return ListView(
-          //shrinkWrap: true,
-
-          children: employers.map( (employer) {
-            return Text(employer.name);
-          }).toList()
+        return ListView.builder(
+            shrinkWrap: true,
+            padding: new EdgeInsets.all(8.0),
+            itemExtent: 20.0,
+            itemCount: employers.length,
+            itemBuilder: (BuildContext context, int index) {
+                return new Text(employers[index].name + employers[index].commissionRate.toString());
+            }
         );
       }
     );
-  }
+  } 
 }
